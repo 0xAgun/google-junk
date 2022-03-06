c4aee0da6d59741062961cef849473f5a4985aab
Ashraful Islam <78228966+0xAgun@users.noreply.github.com>
2022-03-06 11:28:46 +0000

    Update script.py

--- script.py
+++ script.py
@@ -41,31 +41,34 @@ proxie = {
 }
 
 def single_scrap(url, page, proxie):
+    try:
 
-    request = requests.get(url+str(page), proxies=proxie).text
+        request = requests.get(url+str(page), proxies=proxie).text
 
-    soup = BeautifulSoup(request, 'html.parser')
+        soup = BeautifulSoup(request, 'html.parser')
 
-    links = soup.findAll('a')
+        links = soup.findAll('a')
 
-    idnt = "&sa"
-    for x in links:
-        y = x['href']
-        if y.startswith("/url?q="):
-            gg = y.replace("/url?q=", "")
-            hr = gg.split("&sa=")
-            hk = hr[0]
-            gf = hk.split("//")[1]
-            k = gf.split("/")[0]
-            if k in blacklist:
-                pass
-            else:
-                enc = urllib.parse.unquote(hk)
-                print(enc)
-
+        idnt = "&sa"
+        for x in links:
+            y = x['href']
+            if y.startswith("/url?q="):
+                gg = y.replace("/url?q=", "")
+                hr = gg.split("&sa=")
+                hk = hr[0]
+                gf = hk.split("//")[1]
+                k = gf.split("/")[0]
+                if k in blacklist:
+                    pass
+                else:
+                    enc = urllib.parse.unquote(hk)
+                    print(enc)
+    except Exception:
+        pass
+        
+        
 page = 0
 
 for x in range(int_page_num):
     single_scrap(mainurl, page, proxie)
     page += 10
-
